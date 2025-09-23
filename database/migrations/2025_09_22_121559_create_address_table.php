6891a86e9513f16cfa431295bccf1809d1c2769e
DhominicaRiskana <riskanarensa@gmail.com>
2025-09-23 12:20:05 +0700

    Pembuatan File Route API dan Migrasi Alamat, Kecamatan, Kelurahan dan Perubahan Migrasi User

---
+++ database/migrations/2025_09_22_121559_create_address_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kelurahan_id');
+            $table->foreign('kelurahan_id')->references('id')->on('kelurahan')->onDelete('cascade');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
