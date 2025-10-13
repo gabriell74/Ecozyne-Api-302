f6d262149094a004c1f72bbdead21fe2e225570e
gabriell74 <gabrielvivaldi07@gmail.com>
2025-10-13 18:14:30 +0700

    Perbaiki tampilan dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'Dasbor Ecozyne')
 
 @section('content')
-
+<div class="container-fluid py-4" style="background-color: #D6EDDE; min-height: 100vh;">
+    <div class="row">
+        <div class="col-12 col-lg-8">
+            <h5 class="fw-bold mb-3" style="color:#343C6A">Pengguna</h5>
             <div class="row">
                 <div class="col-12 col-md-6 mb-3">
                     <div class="card border-0 shadow-sm rounded-4">
