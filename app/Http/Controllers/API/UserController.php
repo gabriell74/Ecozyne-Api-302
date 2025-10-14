8e83774f75bae87cf0de593694cc5d92751f4a20
DhominicaRiskana <riskanarensa@gmail.com>
2025-10-14 17:40:15 +0700

    Menambahkan Function Edit Profile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -77,60 +77,17 @@ class UserController extends Controller
         }
     }
 
-    /**
-     * Display a listing of the resource.
-     *  
-     */
-    public function index()
+    public function editProfile(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+         $validated = $request->validate([
+                'username' => 'required',
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:8',
+                'phone_number' => 'required|min:10',
+                'address' => 'required',
+                'postal_code' => 'required',
+                'kelurahan' => 'required',
+         ]);
     }
 }
