b58332e1e2dd149cfad48914a2d271ba8fcdd7cf
gabriell74 <gabrielvivaldi07@gmail.com>
2025-11-18 13:42:17 +0700

    Menambah user_id, status, dan rejectionReason fields ke TrashTransaction seeder

--- database/seeders/TrashTransactionTableSeeder.php
+++ database/seeders/TrashTransactionTableSeeder.php
@@ -14,6 +14,9 @@ class TrashTransactionTableSeeder extends Seeder
         for ($i = 1; $i <= 25; $i++) {
             DB::table('trash_transaction')->insert([
                 'waste_bank_id' => $faker->numberBetween(1, 5),
+                'user_id' => $faker->numberBetween(1, 10),
+                'status' => $faker->randomElement(['rejected', 'pending', 'approved']),
+                'rejectionReason' => $faker->optional()->sentence(),
                 'poin_earned' => $faker->numberBetween(10, 100),
                 'trash_weight' => $faker->numberBetween(1, 20),
                 'created_at' => $faker->dateTimeBetween('-2 months', 'now'),
