03ec2f80c8973082ac8417136829aee4848f6aa3
Foundry2us <yusrilaril1@gmail.com>
2025-12-15 13:25:02 +0700

    Perbaiki seeder dikit

--- database/seeders/ProductTransactionTableSeeder.php
+++ database/seeders/ProductTransactionTableSeeder.php
@@ -10,16 +10,22 @@ class ProductTransactionTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create('id_ID');
-        
+
+        $rewards = [
+            'Beras 5 Kg',
+            'Gula 1 Kg',
+            'Minyak 1 Liter',
+            'Jus Coy'
+        ];
+
         for ($i = 1; $i <= 20; $i++) {
             DB::table('product_transaction')->insert([
                 'order_id' => $faker->numberBetween(1, 15),
                 'product_id' => $faker->numberBetween(1, 5),
+                'product_name' => $rewards[array_rand($rewards)],
+                'product_price' => $faker->numberBetween(1, 5),
                 'total_price' => $faker->numberBetween(15000, 100000),
                 'amount' => $faker->numberBetween(1, 5),
-                'order_customer' => $faker->name(),
-                'order_phone_number' => $faker->phoneNumber(),
-                'order_address' => $faker->address(),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
